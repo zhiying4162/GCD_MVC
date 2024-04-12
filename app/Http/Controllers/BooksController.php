24a76958de11fdebb71a1c65183eddb6d5d45c7e
Zhiying <melody9306@gmail.com>
2024-04-13 00:59:17 +0800

    update new Recursion

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -10,14 +10,14 @@ class BooksController extends Controller
     {
         return view('index');
     }
-
+  
     public function calculate(Request $request)
     {
+        // 迴圈
         $Num1 = $request->input('Number1');
         $Num2 = $request->input('Number2');
 
         session(['Number1'=>$Num1]);
-
         session(['Number2'=>$Num2]);
 
         do {
@@ -31,16 +31,43 @@ class BooksController extends Controller
 
         return view('index');
     }
+
+    public function Recursion(Request $request)
+    {
+        // 遞迴
+        $Num1_R = $request->input('Number1');
+        $Num2_R = $request->input('Number2');
+
+        session(['Number1'=>$Num1_R]);
+        session(['Number2'=>$Num2_R]);
+
+        if($Num2_R==0){
+           $ans_R = $Num1_R;
+        }
+        return Recursion($Num2_R , $Num1_R % $Num2_R);
+        session(['ans'=>$ans_R]);
+
+        return view('index');
+    }
     
     public function show(Request $request)
     {
-
+        // 迴圈
         $Num1 = session('Number1');
         $Num2 = session('Number2');
-
         $ans = session('ans');
 
+        // 遞迴
+        $Num1_R = session('Number1');
+        $Num2_R = session('Number2');
+        $ans_R = session('ans');
+
+        echo "迴圈<br>";
         echo "$Num1 和 $Num2 的最大公因數為： $ans";
+        echo "<hr>";
+        echo "遞迴<br>";
+        echo "$Num1_R 和 $Num2_R 的最大公因數為： $ans_R";
     }
+    
 }
 
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,4 @@ use App\Http\Controllers\BooksController;
 Route::get('/', [BooksController::class, 'index'])->name('index');
 Route::get('/books/calculate', [BooksController::class, 'calculate'])->name('calculate');
 Route::get('/books/show', [BooksController::class, 'show'])->name('show');
+Route::get('/books/Recursion', [BooksController::class, 'Recursion'])->name('Recursion');
