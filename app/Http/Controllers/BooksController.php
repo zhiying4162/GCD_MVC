18f001bd6c4721cb18c74c5e57d25aa6eb25730a
Zhiying <melody9306@gmail.com>
2024-04-13 01:19:05 +0800

    update BooksController Forloop and Recursion

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -13,13 +13,21 @@ class BooksController extends Controller
   
     public function calculate(Request $request)
     {
-        // 迴圈
         $Num1 = $request->input('Number1');
         $Num2 = $request->input('Number2');
 
         session(['Number1'=>$Num1]);
         session(['Number2'=>$Num2]);
 
+        $this->Forloop($Num1, $Num2);
+        $this->Recursion($Num1, $Num2);
+
+        return view('index');
+    }
+
+    public function Forloop($Num1,$Num2)
+    {
+        // 迴圈
         do {
             $gcd = $Num1 % $Num2;
             $Num1 = $Num2;
@@ -28,45 +36,36 @@ class BooksController extends Controller
 
         $ans = $Num1;
         session(['ans'=>$ans]);
-
-        return view('index');
     }
 
-    public function Recursion(Request $request)
+    public function Recursion($Num1,$Num2)
     {
         // 遞迴
-        $Num1_R = $request->input('Number1');
-        $Num2_R = $request->input('Number2');
-
-        session(['Number1'=>$Num1_R]);
-        session(['Number2'=>$Num2_R]);
-
-        if($Num2_R==0){
-           $ans_R = $Num1_R;
+        if($Num2==0){
+           $ans_R = $Num1;
+           session(['ans_R'=>$ans_R]);
         }
-        return Recursion($Num2_R , $Num1_R % $Num2_R);
-        session(['ans'=>$ans_R]);
-
-        return view('index');
+        else {
+            $this->Recursion($Num2, $Num1 % $Num2);
+        }
     }
     
     public function show(Request $request)
     {
-        // 迴圈
         $Num1 = session('Number1');
         $Num2 = session('Number2');
+
+        // 迴圈
         $ans = session('ans');
 
         // 遞迴
-        $Num1_R = session('Number1');
-        $Num2_R = session('Number2');
-        $ans_R = session('ans');
+        $ans_R = session('ans_R');
 
         echo "迴圈<br>";
         echo "$Num1 和 $Num2 的最大公因數為： $ans";
         echo "<hr>";
         echo "遞迴<br>";
-        echo "$Num1_R 和 $Num2_R 的最大公因數為： $ans_R";
+        echo "$Num1 和 $Num2 的最大公因數為： $ans_R";
     }
     
 }
